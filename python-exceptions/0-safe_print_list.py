e9e5f605d156e9f27c442849e62791201a8c92c6
Pendarium <thomas.sorli21@gmail.com>
2025-09-10 16:02:32 +0200

    task 5

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 
-def raise_exception():
-    raise TypeError("Erreur")
+def safe_print_list(my_list=[], x=0):
---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    raise TypeError("Ceci est une erreur de type")
