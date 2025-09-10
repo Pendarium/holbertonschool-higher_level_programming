06fe0e975dded1d866e361bcc4f1a7da14c8e158
Pendarium <thomas.sorli21@gmail.com>
2025-09-10 12:05:29 +0200

    updt pycode

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -16,5 +16,8 @@ def best_score(a_dictionary):
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary, key=lambda k: a_dictionary[k])
+    return max(
+        a_dictionary,
+        key=lambda k: a_dictionary[k]
+    )
 """
