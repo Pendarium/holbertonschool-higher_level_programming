4d34f7d4009b04e8f04571c157cfe17262f454a2
Pendarium <thomas.sorli21@gmail.com>
2025-09-10 11:57:52 +0200

    task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    values = [romans[char] for char in roman_string]
+
+    total = 0
+    for i in range(len(values)):
+        if i + 1 < len(values) and values[i] < values[i + 1]:
+            total -= values[i]
+        else:
+            total += values[i]
+    return total
