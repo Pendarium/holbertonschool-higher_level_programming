862d1f35684acf0bfcc89f4b3e26a5eddc878a79
Pendarium <thomas.sorli21@gmail.com>
2025-09-12 10:52:32 +0200

    updt pycode with gpt

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,11 +7,14 @@ def list_division(my_list_1, my_list_2, list_length):
             element_1 = my_list_1[i]       # Peut lever IndexError
             element_2 = my_list_2[i]       # Peut lever IndexError
 
-            if not isinstance(element_1, (int, float)) or not isinstance(element_2, (int, float)):
+            is_elem1_number = isinstance(element_1, (int, float))
+            is_elem2_number = isinstance(element_2, (int, float))
+
+            if not is_elem1_number or not is_elem2_number:
                 print("wrong type")
                 result.append(0)
             else:
-                division = element_1 / element_2  # Peut lever ZeroDivisionError
+                division = element_1 / element_2
                 result.append(division)
         except IndexError:
             print("out of range")
