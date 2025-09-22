a95fa1b5cb49088921d55f2cd5974c19a73ba53e
Pendarium <thomas.sorli21@gmail.com>
2025-09-23 01:17:56 +0200

    updt docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 
-
 def lookup(obj):
     """
-    Renvoie la liste des attributs et des méthodes
-    disponibles pour un objet donné.
+    Renvoie la liste des attributs et des
+    méthodes disponibles pour un objet donné.
 
     Args:
-        obj: L'objet dont on veut obtenir la
-        liste des attributs et méthodes.
+        obj: L'objet dont on veut obtenir la liste des attributs et méthodes.
 
     Returns:
-        list: Une liste de chaînes contenant les noms
-        des attributs et méthodes accessibles.
+        list: Une liste de chaînes contenant
+              les noms des attributs et méthodes accessibles.
     """
     return dir(obj)
