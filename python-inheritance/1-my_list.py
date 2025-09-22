b10a69ce3c6519b1dbe6388abe036059b0fec1c1
Pendarium <thomas.sorli21@gmail.com>
2025-09-23 01:29:09 +0200

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module contenant la classe MyList.
+
+Cette classe hérite de list et ajoute une méthode
+pour afficher la liste triée en ordre croissant.
+"""
+
+
+class MyList(list):
+    """
+    Classe héritant de list avec une méthode pour afficher la liste triée.
+
+    Methods:
+        print_sorted(): Affiche la liste triée en ordre croissant.
+    """
+
+    def print_sorted(self):
+        """
+        Affiche la liste triée en ordre croissant.
+
+        Args:
+            self: Instance de la classe MyList.
+
+        Returns:
+            None
+        """
+        print(sorted(self))
