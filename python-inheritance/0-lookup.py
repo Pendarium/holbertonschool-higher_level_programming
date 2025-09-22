6315ad20bb34b7ccfcf7355b543c76bc27d30faf
Pendarium <thomas.sorli21@gmail.com>
2025-09-23 01:16:13 +0200

    task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def lookup(obj):
+    """
+    Renvoie la liste des attributs et des méthodes
+    disponibles pour un objet donné.
+
+    Args:
+        obj: L'objet dont on veut obtenir la
+        liste des attributs et méthodes.
+
+    Returns:
+        list: Une liste de chaînes contenant les noms
+        des attributs et méthodes accessibles.
+    """
+    return dir(obj)
