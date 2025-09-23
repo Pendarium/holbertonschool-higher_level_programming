a75df8e8c629fb30708e922e4ab0f3373acee3cc
Pendarium <thomas.sorli21@gmail.com>
2025-09-23 02:07:25 +0200

    docstring propre

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
+"""
+Module: is_same_class
+
+Description:
+    Module contenant une fonction pour vérifier si un objet
+    est exactement une instance d'une classe donnée.
+"""
 
 
 def is_same_class(obj, a_class):
     """
-Module: is_same_class
+    Vérifie si un objet est exactement une instance d'une classe donnée.
 
-Description:
-    Fonction permettant de vérifier si un objet est exactement
-    une instance d'une classe donnée.
+    Args:
+        obj (object): L'objet à vérifier.
+        a_class (type): La classe à comparer.
 
-Args:
-    obj (object): L'objet à vérifier.
-    a_class (type): La classe à comparer.
-
-Returns:
-    bool: True si l'objet est exactement une instance de la classe,
-          False sinon.
+    Returns:
+        bool: True si l'objet est exactement une instance de la classe,
+              False sinon.
     """
     if obj.__class__ != a_class:
         return False
