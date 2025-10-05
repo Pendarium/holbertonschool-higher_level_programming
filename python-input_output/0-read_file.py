4447af6bb2d09bb00bbe8b287dae967f29df5071
pendarium <thomas.sorli21@gmail.com>
2025-10-05 22:35:26 +0200

    pep

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 """Module contenant la fonction read_file
 qui lit et affiche un fichier texte UTF-8."""
 
+
 def read_file(my_file_0=""):
     """Lit un fichier texte (UTF8) et l’affiche sur la sortie standard."""
     with open(my_file_0, encoding="utf-8") as f:
