dad5d8685941c35b5db507aaae37eae8a1011193
pendarium <thomas.sorli21@gmail.com>
2025-10-20 19:32:29 +0200

    task 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
 -- this script can be use to return all not NULL value right score and name by desc
-SELECT name FROM second_table WHERE name IS NULL AND name != '';
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
