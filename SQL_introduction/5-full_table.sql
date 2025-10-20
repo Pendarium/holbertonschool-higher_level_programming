7e593ca5a47ac34fc94775224e77c6a1a8fa2420
pendarium <thomas.sorli21@gmail.com>
2025-10-20 14:56:02 +0200

    task 5/6

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- this command is use to see who are create first table
+SHOW CREATE TABLE first_table;
---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- this script is use to list all content in first table
+SELECT * FROM first_table;
