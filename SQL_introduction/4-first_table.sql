bdba89d57f9b02c6b38a5d3a6904a0ac09470baa
pendarium <thomas.sorli21@gmail.com>
2025-10-20 14:37:51 +0200

    first table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,4 @@
---this script create a table named VARACHAR(256) and have for ID INT
-CREATE TABLE customers (
-    customer_id INT,
-    name VARCHAR(256)
-);
+-- this script create a table named VARACHAR(256) and have for ID INT
+CREATE TABLE if NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256));
