d508013fd26dfc4ef77b194366a4c63a2142251b
pendarium <thomas.sorli21@gmail.com>
2025-10-20 17:07:06 +0200

    task9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- this script create table and add value 
+CREATE TABLE if NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) value (1, 'John', 10);
+INSERT INTO second_table (id, name, score) value (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) value (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) value (4, 'George', 8);
