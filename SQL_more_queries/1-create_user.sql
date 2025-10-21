d83db2f3889a220a913ed9df086c84d128596a12
pendarium <thomas.sorli21@gmail.com>
2025-10-21 15:54:50 +0200

    task 0/1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script creates the user if not existing and grants all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
