6e87324939adbbbcdf533506075f5d2d564a851f
pendarium <thomas.sorli21@gmail.com>
2025-10-26 23:46:02 +0100

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -26,7 +26,8 @@ if __name__ == '__main__':
     # Création du curseur et exécution de la requête SQL
     curseur = db.cursor()
     curseur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+            etat_recherche)
     )
 
     # Affichage des résultats
