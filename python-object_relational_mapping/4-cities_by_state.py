9bad5201addb3fb513b78ffd733591058359eee1
pendarium <thomas.sorli21@gmail.com>
2025-10-27 00:01:19 +0100

    task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
 """
-This module contains a script that does a data
-request to database
+Liste toutes les villes de la base de données hbtn_0e_4_usa.
+Usage :
+    ./script.py <mysql_user> <mysql_password> <database>
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
 
-    # function to connect to a MySQL database
+    # Connexion à la base MySQL
     db = MySQLdb.connect(
-        host="localhost", user=username, passwd=password,
-        db=database, port=3306
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=db_name
     )
+
     cur = db.cursor()
-    # créer un curseur - permet d'avoir plusieurs environnement
-    # sur la même connexion à la DB ?
-
-    cur.execute(
-        "SELECT c.id, c.name, s.name FROM cities AS c "
-        "LEFT JOIN states AS s ON s.id = c.state_id")
-    # envoie une requête
+    # Récupérer toutes les villes triées par id
+    cur.execute("SELECT name FROM cities ORDER BY id ASC")
     rows = cur.fetchall()
-    # récupère les lignes renvoyées par la base
-    # et les stock en liste de tuples
-    # on peut récupérer une ligne à la fois ou un nombre n de lignes
-    # avec fetchone() et fetchmany(n)
 
-    for element in rows:
-        print(element)
+    # Affichage sous forme de ligne séparée par des virgules
+    city_names = [row[0] for row in rows]
+    print(", ".join(city_names))
 
     cur.close()
     db.close()
