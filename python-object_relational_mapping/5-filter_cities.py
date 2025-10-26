8ee9fa22493e1b22e30e7e7cf04af2c3b5e1a6ae
pendarium <thomas.sorli21@gmail.com>
2025-10-27 00:00:09 +0100

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
 """
-This module contains a script that does a data
-request to database
+Affiche toutes les villes d'un état (nom passé en argument) à partir
+de la base de données hbtn_0e_4_usa.
+
+Usage:
+    ./script.py <mysql_user> <mysql_password> <database> <state_name>
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state = sys.argv[4]
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    # function to connect to a MySQL database
     db = MySQLdb.connect(
-        host="localhost", user=username, passwd=password,
-        db=database, port=3306
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=db_name
     )
-    cur = db.cursor()
-    # créer un curseur - permet d'avoir plusieurs environnement
-    # sur la même connexion à la DB ?
 
+    cur = db.cursor()
     cur.execute(
         "SELECT c.name FROM cities AS c "
-        "LEFT JOIN states AS s ON s.id = c.state_id "
-        "WHERE s.name LIKE BINARY %s", (state,))
-    # envoie une requête
-    rows = cur.fetchall()
-    # récupère les lignes renvoyées par la base
-    # et les stock en liste de tuples
-    # on peut récupérer une ligne à la fois ou un nombre n de lignes
-    # avec fetchone() et fetchmany(n)
+        "JOIN states AS s ON s.id = c.state_id "
+        "WHERE s.name = %s "
+        "ORDER BY c.id ASC",
+        (state_name,)
+    )
 
+    rows = cur.fetchall()
     city_names = [row[0] for row in rows]
     print(", ".join(city_names))
 
