5065e1554d59c6f10c775165f8c91afa61a370e0
pendarium <thomas.sorli21@gmail.com>
2025-11-16 19:24:35 +0100

    add files + tadk project 1

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('add_item').addEventListener('click', function () {
+        const li = document.createElement('li');
+        li.textContent = 'Item';
+
+        document.querySelector('.my_list').appendChild(li);
+    });
+});
