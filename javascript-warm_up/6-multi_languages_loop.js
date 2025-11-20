a4043c7fc4474e9bd20920929c7462c10abdeceb
pendarium <thomas.sorli21@gmail.com>
2025-11-20 15:02:20 +0100

    warm up js

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Tableau des lignes
+const languages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Construire un tableau temporaire pour ajouter les retours à la ligne
+const output = [];
+for (let i = 0; i < languages.length; i++) {
+  output.push(languages[i]); // ajouter chaque ligne
+}
+
+// Afficher tout avec une seule console.log
+console.log(output.join('\n'));
---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else if (x > 0) {
+  const output = [];
+  for (let i = 0; i < x; i++) {
+    output.push('C is fun');
+  }
+  console.log(output.join('\n'));
+}
